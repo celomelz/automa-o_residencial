c47a140b8872142cf5a78ddd588082d02475f146
celomelz <marcelo_melz@estudante.sc.senai.br>
2016-09-21 15:25:09 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -3,12 +3,12 @@ function AlteraRele1(){
     if(estado === "0"){
         document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
-		document.getElementById("botao").innerHTML="<a href='/?desligar' 
+		document.getElementById("botao").innerHTML="<a href='/?desligar'
 class='botao'>Desliga</a>";
     } else {
         document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
-        document.getElementById("botao").innerHTML="<a href='/?ligar' 
+        document.getElementById("botao").innerHTML="<a href='/?ligar'
 class='botao'>Liga</a>";
     }
 }
